4128b6ff411be304a5f99fae0c26a6778e50601f
Tahmeed Ferdous <tahmeed.ferdous@g.bracu.ac.bd>
2024-12-20 10:31:27 +0600

    Update urls.py

--- firstApp/urls.py
+++ firstApp/urls.py
@@ -3,6 +3,7 @@ from .views import firstone, delete_stu, update
 
 urlpatterns = [
     path('',firstone, name='firstone'),
+    
     path('delete_stu/<int:id>/', delete_stu,name='delete_stu'),
     path('update/<int:id>/', update,name='update')
 ]
